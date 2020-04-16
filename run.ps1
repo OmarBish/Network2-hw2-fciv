55eb7ce0c43233ada05b122860e68b43cb609422
Omar Bishtawi <omar.f.bishtawi@gmail.com>
2020-04-16 21:26:24 +0300

    add command lines

--- run.ps1
+++ run.ps1
@@ -18,6 +18,7 @@ Write-Host "
                                            
 "
 Write-Host "Generate MD5, and SHA1 then Store the resulting SHA1 and MD5 in an XML file"
+Write-Host "command:- ./fciv.exe tst.txt -sha1 -xml db.xml "
 Write-Host "
 
 /_____/_____/_____/_____/_____/_____/_____/
@@ -30,18 +31,31 @@ Write-Host "
                                            
 "
 Write-Host "Check the validity of the generated file"
+Write-Host "command:- ./fciv.exe -v -sha1 -xml db.xml"
 Write-Host "
 
 /_____/_____/_____/_____/_____/_____/_____/
                                            
 "
 ./fciv.exe -v -sha1 -xml db.xml
+
+
+Write-Host "db.xml content"
+Write-Host "
+
+/_____/_____/_____/_____/_____/_____/_____/
+                                           
+"
+more ./db.xml
+
 Write-Host "
 
 /_____/_____/_____/_____/_____/_____/_____/
                                            
 "
 Write-Host "Modify the file:- "
+Write-Host "command:-Add-Content ./tst.txt 'New Text'"
+Write-Host "command:-git diff"
 Write-Host "
 
 /_____/_____/_____/_____/_____/_____/_____/
@@ -57,6 +71,7 @@ Write-Host "
                                            
 "
 Write-Host "Recheck the validity after update"
+Write-Host "command:- ./fciv.exe -v -sha1 -xml db.xml"
 Write-Host "
 
 /_____/_____/_____/_____/_____/_____/_____/
